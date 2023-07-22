4b79244a3d6426e59f37e70bdc70028f771092b8
kasinath777 <74859052+kasinath777@users.noreply.github.com>
2023-07-22 14:46:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+console.log("this is console screen");
+
+var firstName="kasinath ";
+var secondName="TALLURY";
+console.log(firstName);
+console.log(secondName);
+var fullName=firstName.concat(secondName);
+console.log(firstName+secondName);
+console.log(fullName);
+
+var length= fullName.length;
+console.log(length);
+
+var sentence="I am a boy";
+replaceSentence= sentence.replace("I","Rohan");
+console.log(sentence);
+console.log(replaceSentence);
+
+var capitalName= firstName.toUpperCase();
+console.log(capitalName);
+var smallName= secondName.toLowerCase();
+console.log(smallName);
+
+var squareRoot= Math.sqrt(100);
+console.log(squareRoot);
+
+var maxNumber=Math.max(20,9999,6900,7200);
+var minNum=Math.min(30,500);
+console.log(maxNumber);
+console.log(minNum);
+
+var power=Math.pow(8,3);
+console.log(power);
+
+var number=0.1234567890123;
+var num1=number.toFixed(5);
+console.log(number);
+console.log(num1);
+
+var num2=60;
+var num3=90;
+console.log(num2+num3);
+
+var num4=num2.toString();
+var num5=num3.toString();
+console.log(num4+num5);
+
+var type1=999;
+console.log(typeof type1)
